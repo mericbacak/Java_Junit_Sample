b00502ba7aabea32ede61162fa068bcf900250ab
meric <mericbcak@gmail.com>
2023-06-14 22:21:45 +0300

    java junit samples

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Calculator {
+    public double toplama(double a,double b){
+        return a+b;
+    }
+
+    public double cikarma(double a,double b){
+        return a-b;
+    }
+
+    public double carpma(double a,double b){
+        return a*b;
+    }
+
+}
