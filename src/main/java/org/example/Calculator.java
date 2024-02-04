c66ee95a0cb5dda028d0778ace1eccf22822e925
meric.bacak <mericbacak@viennalife.com>
2024-02-04 19:34:11 +0300

    Junit examples added and modified sunnyNumber tests added

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,16 +1,13 @@
 package org.example;
 
 public class Calculator {
-    public double toplama(double a,double b){
-        return a+b;
-    }
-
-    public double cikarma(double a,double b){
-        return a-b;
-    }
-
-    public double carpma(double a,double b){
-        return a*b;
-    }
+    public static int add(int a, int b){return a+b;}
+    public static double add(double a, double b){return a+b;}
+    public static double subtraction(double a, double b){ return a-b; }
+    public static int subtraction(int a, int b){ return a-b; }
+    public static double multiply(double a, double b){return a*b;}
+    public static int multiply(int a, int b){return a*b;}
+    public static double dividion(double a, double b){return a/b;}
+    public static int dividion(int a, int b){return a/b;}
 
 }
